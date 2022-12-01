8f89f9027f6a92c95dc617136ef0684592e864cc
Cocisran <fcoemmdmm@ciencias.unam.mx>
2022-12-01 11:38:13 -0600

    arquitectura del nodo principal

--- control/control/cerebro.py
+++ control/control/cerebro.py
@@ -4,34 +4,38 @@ from rclpy.node import Node
 from std_msgs.msg import String
 
 
-class MinimalPublisher(Node):
+class ControlNode(Node):
 
     def __init__(self):
-        super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        super().__init__('ControlNode')
+        #Informacion sobre el proceso de ejecución
+        self.publisher_ = self.create_publisher(String, 'control_log', 10)
 
-    def timer_callback(self):
+    def print_log(self,log_msg = ''):
         msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        msg.data = f"SAlEM: {log_msg}"
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+
+    def go_salem(self):
+        '''
+        Aqui se establecerá el flujo de control principal de
+        la rutina de ejecución de salem.
+        '''
+        pass
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_publisher = MinimalPublisher()
-
-    rclpy.spin(minimal_publisher)
-
+    salem_brain = ControlNode()
+    salem_brain.go_salem()
+    rclpy.spin(salem_brain)
+    
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    salem_brain.destroy_node()
     rclpy.shutdown()
 
 
