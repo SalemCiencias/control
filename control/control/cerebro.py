d85324f6c87009b1493fa9f4c51c6f3f5a7548f5
xKary <karyxprado@ciencias.unam.mx>
2023-03-26 18:02:41 -0600

    moviemiento

--- control/control/cerebro.py
+++ control/control/cerebro.py
@@ -6,8 +6,8 @@ from rclpy.callback_groups import MutuallyExclusiveCallbackGroup, ReentrantCallb
 
 
 from cui_interfaces.srv import SoundRequest
+from navegacion_interfaces.action import Navegacion
 from action_cheese.action import Cheese
-
 from .manejoClientes import ActionClientManager, ClientAsync
 import os
 import sys
@@ -18,13 +18,41 @@ from std_msgs.msg import String
 
 CHEESE_CAMERA = int(sys.argv[1])
 
-class CheeseClient(ActionClientManager):
+class NavegacionClient(ActionClientManager):
 
-    ALL_OK = 'YES'
-    def __init__(self,node,action_type, action_name, cui_service):
+    def __init__(self,node,action_type, action_name, cui_service = None):
         group = ReentrantCallbackGroup()
         self.client = ActionClient(node, action_type, action_name,callback_group=group)
         self.cui = cui_service
+        self.node = node
+        self.__goal_future = None
+        self.__result_future = None
+
+    def build_goal_msg(self,*args):
+        goal_msg = Navegacion.Goal()
+        goal_msg.x = args[0] 
+        goal_msg.y = args[1]
+        return goal_msg
+
+    def result_callback(self, future):
+        result = future.result()
+        print(result)
+        
+
+    def feedback_callback(self, feedback_msg):
+        feedback = feedback_msg.feedback
+        message = 'Received feedback: {0}'.format(feedback.progress)
+        print(message)
+
+
+class CheeseClient(ActionClientManager):
+
+    ALL_OK = 'YES'
+    def __init__(self,node,action_type, action_name, cui_service, nav_service):
+        group = ReentrantCallbackGroup()
+        self.client = ActionClient(node, action_type, action_name,callback_group=group)
+        self.cui = cui_service
+        self.navegacion = nav_service
         self.node = node
         self.__goal_future = None
         self.__result_future = None
@@ -41,16 +69,13 @@ class CheeseClient(ActionClientManager):
         if result == self.ALL_OK:
             self.lock = False
             Popen(f'ros2 run face_recognition_pub talker {CHEESE_CAMERA}',shell=True)
-            print('aqui ************************************************************')
             self.cui.build_request(
-            'n', 'Prueba de que funicona')
+            'n', 'Iniciando autodestrucción')
             self.cui.send_request()
 
-            print("Seguimos con la ejecucion")
-
-            self.cui.build_request(
-                'n', 'otra cosa')
-            self.cui.send_request()
+            # self.build_goal_msg('5','1')
+            # self.navegacion.send_request()
+            Popen("ros2 action send_goal --feedback mover navegacion_interfaces/action/Navegacion '{x: 5, y: 1}'",shell=True)
         else:
             print('Se intentará tomar de nuevo las fotos')
             self.sendlock_thread_goal('inicia')
@@ -58,10 +83,8 @@ class CheeseClient(ActionClientManager):
         
 
     def feedback_callback(self, feedback_msg):
-        self.cuenta += 1
         feedback = feedback_msg.feedback
         message = 'Received feedback: {0}'.format(feedback.progress)
-        lock_thread = True if self.cuenta == 5 else False
         print(message)
 
 class SalemVoiceClient(ClientAsync):
@@ -77,12 +100,10 @@ class ControlNode(Node):
         super().__init__('ControlNode')
         self.voz_salem = SalemVoiceClient(
             'Voz_Salem', SoundRequest, 'sound_request')
-
-        self.cheese_client = CheeseClient(self,Cheese, 'cheese',cui_service=self.voz_salem)    
-
+        self.navegacion = NavegacionClient(self, Navegacion, 'mover')
+        self.cheese_client = CheeseClient(self,Cheese, 'cheese',cui_service=self.voz_salem, nav_service = self.navegacion)    
 
         self.publisher = self.create_publisher(String, 'control_log', 10)
-
 
 
     def print_log(self, log_msg=''):
